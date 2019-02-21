37a57a061434fd4cc67d425b8dce8ccd4528c049
unknown <haritha.bapanpally@gmail.com>
2019-02-21 22:59:53 +0530

    add test method in helloworld

--- GitDemoProject/src/com/test/HelloWorld.java
+++ GitDemoProject/src/com/test/HelloWorld.java
@@ -1,6 +1,11 @@
 package com.test;
 
 public class HelloWorld {
+	
+	public void test(){
+		
+		System.out.println("test");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Main method");
