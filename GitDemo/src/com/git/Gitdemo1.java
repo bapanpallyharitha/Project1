6676bffdf932d1b29ed5d26b1b10d7377da4c379
unknown <haritha.bapanpally@gmail.com>
2019-02-22 13:38:32 +0530

    added branch2

---
+++ GitDemo/src/com/git/Gitdemo1.java
@@ -0,0 +1,13 @@
+package com.git;
+
+public class Gitdemo1 {
+	public void gitp(){
+		System.out.println("test");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
